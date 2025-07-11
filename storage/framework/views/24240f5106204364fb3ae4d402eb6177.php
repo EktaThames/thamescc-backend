8d24cef10f7c01ba86ce90211e79b53889186382
Ekta Yadav <ekta.thames@gmail.com>
2025-07-11 13:34:14 +0530

    test

--- storage/framework/views/24240f5106204364fb3ae4d402eb6177.php
+++ storage/framework/views/24240f5106204364fb3ae4d402eb6177.php
@@ -108,6 +108,18 @@ unset($__defined_vars); ?>
         <style>
             <?php echo core()->getConfigData('general.content.custom_scripts.custom_css'); ?>
 
+            button.secondary-button,
+            .secondary-button {
+                background-color: #ff6c2a !important;
+                border: none !important;
+                color: #fff !important;
+            }
+            button.primary-button,
+            .primary-button {
+                background-color: #ff6c2a !important;
+                border: 1px solid #ff6c2a !important;
+                color: #fff !important;
+            }
         </style>
 
         <?php echo view_render_event('bagisto.shop.layout.head.after'); ?>
