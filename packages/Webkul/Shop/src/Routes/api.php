e8291b8161d040a241c57e8ea320e3b62da95ca4
Ekta Yadav <ekta.thames@gmail.com>
2025-07-16 20:08:10 +0530

    api file changes

--- packages/Webkul/Shop/src/Routes/api.php
+++ packages/Webkul/Shop/src/Routes/api.php
@@ -12,6 +12,8 @@ use Webkul\Shop\Http\Controllers\API\ProductController;
 use Webkul\Shop\Http\Controllers\API\ReviewController;
 use Webkul\Shop\Http\Controllers\API\WishlistController;
 
+//
+
 Route::group(['prefix' => 'api','middleware' => 'api'], function () {
     Route::controller(CoreController::class)->prefix('core')->group(function () {
         Route::get('countries', 'getCountries')->name('shop.api.core.countries');
